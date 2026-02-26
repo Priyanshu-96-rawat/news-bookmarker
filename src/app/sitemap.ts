c2a37b4c7095f1b18c26c98034ce36730403af9a
Priyanshu Rawat <priyanshurawat655@gmail.com>
2026-02-26 20:41:22 +0530

    Add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,17 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: 'https://news-bookmarker.vercel.app',
             lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 1,
         },
-    ]
+        {
+            url: 'https://news-bookmarker.vercel.app/login',
+            lastModified: new Date(),
+            priority: 0.5,
+        },
+    ];
 }
